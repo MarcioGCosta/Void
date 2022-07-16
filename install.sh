087f0b1ce3a5125d9f9b3743e04ae0b026c297ee
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2022-07-16 16:54:00 -0300

    Making this script usable

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo xbps-install -Su picom  setxkbmap xrandr elogind feh zsh-syntax-highlighting zsh-autosuggestions libvdpau-va-gl curl make pulsemixer qbittorrent bspwm redshift sxhkd xorg-server-common xorg-minimal ufw xinit polkit polkit-elogind dbus-elogind ntp wireplumber firefox kitty mpv yt-dlp pipewire papirus-icon-theme alsa-pipewire libva-intel-driver rofi playerctl dash go zsh noto-fonts-emoji font-ibm-plex-ttf xorg-fonts neovim opendoas yarn pnpm wl-clipboard xf86-video-intel
 
 ## Moving folders
-mv $HOME/Void/config/.config/ $HOME/.config && mv $HOME/Void/config/.zshrc /$HOME/.zshrc && mv $HOME/Void/background/* $HOME/.config/ && mv $HOME/Void/config/.zshnev /$HOME/.zshnev && mv $HOME/Void/config/.xinitrc $HOME/.xinitrc
+mv $HOME/Void/config/.config/ $HOME/.config && mv $HOME/Void/config/.zshrc /$HOME/.zshrc && mv $HOME/Void/background/ $HOME/.config/ && mv $HOME/Void/config/.zshnev /$HOME/.zshnev && mv $HOME/Void/config/.xinitrc $HOME/.xinitrc 
 
 ## Installing vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
