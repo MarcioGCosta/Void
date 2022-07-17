77d21bdac9e60ca7e1ba5be8ad781a8870eb4edc
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2022-07-17 10:35:13 -0300

    Added nerd-fonts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Installing misc progams
-sudo xbps-install -Su picom polybar setxkbmap xrandr elogind feh zsh-syntax-highlighting zsh-autosuggestions libvdpau-va-gl curl make pulsemixer qbittorrent bspwm redshift sxhkd xorg-server-common xorg-minimal ufw xinit polkit polkit-elogind dbus-elogind ntp wireplumber firefox kitty mpv yt-dlp pipewire papirus-icon-theme alsa-pipewire libva-intel-driver rofi playerctl dash go zsh noto-fonts-emoji font-ibm-plex-ttf xorg-fonts neovim opendoas yarn pnpm wl-clipboard xf86-video-intel
+sudo xbps-install -Su nerd-fonts-ttf picom polybar setxkbmap xrandr elogind feh zsh-syntax-highlighting zsh-autosuggestions libvdpau-va-gl curl make pulsemixer qbittorrent bspwm redshift sxhkd xorg-server-common xorg-minimal ufw xinit polkit polkit-elogind dbus-elogind ntp wireplumber firefox kitty mpv yt-dlp pipewire papirus-icon-theme alsa-pipewire libva-intel-driver rofi playerctl dash go zsh noto-fonts-emoji font-ibm-plex-ttf xorg-fonts neovim opendoas yarn pnpm wl-clipboard xf86-video-intel
 
 ## Moving folders
 mv $HOME/Void/config/.config/ $HOME/.config && mv $HOME/Void/config/.zshrc /$HOME/.zshrc && mv $HOME/Void/background/ $HOME/.config/ && mv $HOME/Void/config/.zshnev /$HOME/.zshnev && mv $HOME/Void/config/.xinitrc $HOME/.xinitrc 
