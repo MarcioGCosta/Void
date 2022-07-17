185dfc597e4741755b5c5fa7af89e53b454f619d
MarcioGCosta <marcio.gustavo.2006@gmail.com>
2022-07-17 15:49:28 -0300

    Changed the repo name

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 sudo xbps-install -Su nerd-fonts-ttf picom polybar setxkbmap xrandr elogind feh zsh-syntax-highlighting zsh-autosuggestions libvdpau-va-gl curl make pulsemixer qbittorrent bspwm redshift sxhkd xorg-server-common xorg-minimal ufw xinit polkit polkit-elogind dbus-elogind ntp wireplumber firefox kitty mpv yt-dlp pipewire papirus-icon-theme alsa-pipewire libva-intel-driver rofi playerctl dash go zsh noto-fonts-emoji font-ibm-plex-ttf xorg-fonts neovim opendoas yarn pnpm wl-clipboard xf86-video-intel
 
 ## Moving folders
-mv $HOME/Void/config/.config/ $HOME/.config && mv $HOME/Void/config/.zshrc /$HOME/.zshrc && mv $HOME/Void/background/ $HOME/.config/ && mv $HOME/Void/config/.zshnev /$HOME/.zshnev && mv $HOME/Void/config/.xinitrc $HOME/.xinitrc 
+mv $HOME/Vrice/config/.config/ $HOME/.config && mv $HOME/Vrice/config/.zshrc /$HOME/.zshrc && mv $HOME/Vrice/background/ $HOME/.config/ && mv $HOME/Vrice/config/.zshnev /$HOME/.zshnev && mv $HOME/Vrice/config/.xinitrc $HOME/.xinitrc 
 
 ## xorg  configuration
-sudo mv $HOME/Arch-Linux/config/20-intel-gpu.conf /etc/X11/xorg.conf.d/
+sudo mv $HOME/Vrice/config/20-intel-gpu.conf /etc/X11/xorg.conf.d/
 
 ## Installing vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
